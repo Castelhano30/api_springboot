75d114e8e7913e504d8d604713499d7e4b7982f3
Felipe Castelhano <felipcastelhanogh@gmail.com>
2023-09-27 16:14:59 -0300

    Adicionando dependencias swagger

--- src/main/java/com/trabalhospring/api/controller/LutaController.java
+++ src/main/java/com/trabalhospring/api/controller/LutaController.java
@@ -9,11 +9,15 @@ import com.trabalhospring.api.model.Lutador;
 import com.trabalhospring.api.repository.LutaRepository;
 import com.trabalhospring.api.repository.LutadorRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/lutador/{idLutador}/lutas")
+@Tag (name = "Luta", description = "API DE LUTAS")
 public class LutaController {
 
     private final LutaRepository lutaRepository;
@@ -27,16 +31,19 @@ public class LutaController {
     }
 
     @GetMapping
+     @Operation(summary = "Buscando todas as lutas", method = "GET")
     public List<Luta> getAllLutas() {
         return lutaRepository.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscando lutas por ID", method = "GET")
     public Luta getLutaById(@PathVariable Long id) {
         return lutaRepository.findById(id).orElse(null);
     }
 
     @PostMapping()
+    @Operation(summary = "Criando luta", method = "POST")
     public ResponseEntity<Luta> createLuta(@PathVariable("idLutador") Long idLutador, @RequestBody Luta luta) {
         try {
             // Primeiro, verifique se o lutador existe pelo ID.
@@ -55,6 +62,7 @@ public class LutaController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizando luta", method = "PUT")
     public Luta updateLuta(@PathVariable Long id, @RequestBody Luta updatedLuta) {
         return lutaRepository.findById(id)
                 .map(luta -> {
@@ -66,6 +74,7 @@ public class LutaController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletando luta", method = "DELETE")
     public void deleteLuta(@PathVariable Long id) {
         lutaRepository.deleteById(id);
     }
